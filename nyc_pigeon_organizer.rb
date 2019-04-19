29eae049a3698c2182ac5f7b1ccce7edf80dc33c
Jasterix C <loune.calixte@gmail.com>
2019-04-19 04:10:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,10 +9,11 @@ end
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.group_by do |k,v|
-  data.group_by do |k,v|
-  if new_hash[color] == nil
-    new_hash[color] = {name: type[:type], style: [style]}
-    
+  data.group_by do |key,value|
+  if new_hash[key] == nil
+    binding.pry
+    new_hash[key] = {name: value[:type], style: [style]}
+  end
     
 
     end
