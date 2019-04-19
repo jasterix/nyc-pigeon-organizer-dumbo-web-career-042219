daf886a8632977620ac63baef76ec1dc0490a392
Jasterix C <loune.calixte@gmail.com>
2019-04-19 17:03:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,26 @@
 require 'pry'
 
-def pigeons(data)
-  pigeons = []
-    pigeons << data.values.flatten.uniq
-  end
+# def pigeons(data)
+#   pigeons = []
+#     pigeons << data.values.flatten.uniq
+#   end
   
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  names =  data.values.flatten.uniq
-  binding.pry
-  # data.each |keys, value|
+  names = []
+  data.each do |key, values|
+    names = values.values.flatten.uniq
+    values.each do |keys, options|
+      new_hash[keys]= names
+      binding.pry
+      if data[:color].include?"Theo"
+        new_hash[key]= {color => data[:color], lives => data[:lives]}
+      end
+    end
+  end
+end
+  
+  
   #   if new_hash[key] == nil
   #     # new_hash[key] = new_hash[0] {name: options]
   #     new_hash.keys = names
@@ -18,4 +29,21 @@ def nyc_pigeon_organizer(data)
   #     binding.pry
   #   end
   # end
-end
+
+  # {
+  # :color => {
+  #   :purple => ["Theo", "Peter Jr.", "Lucky"],
+  #   :grey => ["Theo", "Peter Jr.", "Ms. K"],
+  #   :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+  #   :brown => ["Queenie", "Alex"]
+  # },
+  # :gender => {
+  #   :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+  #   :female => ["Queenie", "Ms. K"]
+  # },
+  # :lives => {
+  #   "Subway" => ["Theo", "Queenie"],
+  #   "Central Park" => ["Alex", "Ms. K", "Lucky"],
+  #   "Library" => ["Peter Jr."],
+  #   "City Hall" => ["Andrew"]
+  # }
